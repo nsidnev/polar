ae006117b41403454dcdd98a8eb3584d6a54d131
Nik Sidnev <nik.sidnev@vercel.com>
2026-04-15 19:33:34 -0400

    remove a lot of boilerplate

--- server/vc-services/cron/tasks/expire_checkouts.py
+++
@@ -1,6 +0,0 @@
-import dramatiq
-
-from polar.checkout.tasks import expire_open_checkouts  # noqa: F401
-
-if __name__ == "__main__":
-    dramatiq.get_broker().get_actor("checkout.expire_open_checkouts").send()
